83a38ee170bbc0abb39a2d73f5d06d292fa1ece6
Martin <Martinkv05@gmail.com>
2023-01-10 13:59:31 +0100

    init commit

---
+++ App.js
@@ -0,0 +1,21 @@
+let fodt = new Date(2005,9,15)
+let currentDate = new Date();
+let ageInMilliseconds = currentDate - fodt;
+let ageInYears = ageInMilliseconds / 31536000000;
+let alder = Math.floor(ageInYears) + "år";
+document.getElementById("aar").innerHTML = alder;
+
+
+function drop(pilnr, innholdnr){
+  console.log(pilnr);
+  const Innhold = document.getElementById(innholdnr)
+  const pil = document.getElementById(pilnr)
+    
+  if (Innhold.style.maxHeight === "0px") {
+    Innhold.style.maxHeight = "500px";
+    pil.style.transform = "rotate(-90deg)";
+  } else {
+    Innhold.style.maxHeight = "0px";
+    pil.style.transform = "rotate(0deg)";
+  }
+}
